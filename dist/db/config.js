356a54eb6260fb8372e3117a8e3eb5c451a8c74f
Sam Petering <samuel.petering@gmail.com>
2024-09-28 10:11:45 -0400

    rm .env

--- dist/db/config.js
+++ dist/db/config.js
@@ -1,6 +1,4 @@
-import dotenv from 'dotenv';
 import { requireVariable } from '../util.js';
-dotenv.config({ path: '.env.local' });
 export const CONNECTION_CFG = {
     host: requireVariable('DB_HOST'),
     user: requireVariable('DB_USER'),
--- src/db/config.ts
+++ src/db/config.ts
@@ -1,7 +1,5 @@
-import dotenv from 'dotenv';
 import { requireVariable } from '../util.js';
 
-dotenv.config({ path: '.env.local' });
 export const CONNECTION_CFG = {
   host: requireVariable('DB_HOST'),
   user: requireVariable('DB_USER'),
